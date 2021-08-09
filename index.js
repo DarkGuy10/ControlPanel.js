86bee2d252a68fd301785571cfd47ce1f54dd737
DarkGuy10 <HarperBurn@protonmail.ch>
2021-08-09 09:50:14 +0530

    exporting manager classes

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const Client = require("./classes/Client")
 const Server = require("./classes/Server")
+const ServerManager = require("./classes/ServerManager")
 const User = require("./classes/User")
+const UserManager = require("./classes/UserManager")
 const Voucher = require("./classes/Voucher")
+const VoucherManager = require("./classes/VoucherManager")
 
 module.exports = {
     Client,
     User,
     Server,
-    Voucher
+    Voucher,
+    ServerManager,
+    UserManager,
+    VoucherManager
 }
