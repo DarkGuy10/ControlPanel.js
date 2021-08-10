8e165b37534614a27c2fff5c5c5f27c65a9ebf2b
DarkGuy10 <HarperBurn@protonmail.ch>
2021-08-10 12:09:56 +0530

    Added server data

--- classes/Server.js
+++ classes/Server.js
@@ -8,7 +8,33 @@ class Server{
      * @param {Client} client The client being used
      */
     constructor(data, client){
-        Object.assign(this, data)
+        this.id = data.id
+        this.name = data.name
+        this.description = data.description
+        this.suspended = data.suspended
+        this.identifier = data.identifier
+        this.pterodactyl_id = data.pterodactyl_id
+        this.user_id = data.user_id
+        this.product_id = data.product_id
+        this.created_at = data.created_at
+        this.updated_at = data.updated_at
+        this.product = data.product ? {
+            id:data.product.id,
+            name: data.product.name,
+            description: data.product.description,
+            price: data.product.price,
+            memory: data.product.memory,
+            cpu: data.product.cpu,
+            swap: data.product.swap,
+            disk: data.product.disk,
+            io: data.product.io,
+            databases: data.product.databases,
+            backups: data.product.backups,
+            allocations: data.product.allocations,
+            created_at: data.product.created_at,
+            updated_at: data.product.updated_at,
+            disabled: data.product.disabled
+        } : undefined
         instance = client.instance
     }
 
